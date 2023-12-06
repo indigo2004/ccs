250917e29879f36ab5693da4709959196e77395d
indigo2004 <148696539+indigo2004@users.noreply.github.com>
2023-12-06 16:31:54 +0530

    Update new.py

--- new.py
+++ new.py
@@ -2,15 +2,19 @@ import time
 import picamera
 
 def capture_high_quality_image(output_path='high_quality_image.jpg', resolution=(3280, 2464)):
-    with picamera.PiCamera() as camera:
-        # Set the camera resolution to the maximum supported resolution
-        camera.resolution = resolution
+    try:
+        with picamera.PiCamera() as camera:
+            # Set the camera resolution to the maximum supported resolution
+            camera.resolution = resolution
 
-        # Wait for the automatic gain control to settle
-        time.sleep(2)
+            # Wait for the automatic gain control to settle
+            time.sleep(2)
 
-        # Capture the image
-        camera.capture(output_path)
+            # Capture the image
+            camera.capture(output_path)
+        print(f"High-quality image captured and saved to {output_path}")
+    except Exception as e:
+        print(f"Error: {e}")
 
 if __name__ == "__main__":
     # Replace 'high_quality_image.jpg' with your desired output file name
@@ -18,4 +22,3 @@ if __name__ == "__main__":
 
     # Capture a high-quality image
     capture_high_quality_image(output_image_path)
-    print(f"High-quality image captured and saved to {output_image_path}")
